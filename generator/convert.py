f876226c587d083bc27067cf93d6f75516279b1f
diegolinnpr <diegolinnpr@gmail.com>
2026-02-23 15:18:45 -0800

    reordering

---
+++ generator/convert.py
@@ -0,0 +1,14 @@
+#import numpy as np
+
+#def convert(path):
+#	data = np.load(f"{path}.npz")
+#	points = data["points"]
+#	points = np.ascontiguousarray(data["points"], dtype=np.float32)
+##	points.tofile(path + ".bin")
+#
+#if __name__ == "__main__":
+#	convert("/Users/tobiaswatters/Desktop/DSproj/point_data/cube")
+#	convert("/Users/tobiaswatters/Desktop/DSproj/point_data/dodecahedron")
+#	convert("/Users/tobiaswatters/Desktop/DSproj/point_data/icosahedron")
+#	convert("/Users/tobiaswatters/Desktop/DSproj/point_data/octohedron")
+#	convert("/Users/tobiaswatters/Desktop/DSproj/point_data/tetrahedron")
--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://unpkg.com/three@0.182.0/build/three.module.js";
+/*import * as THREE from "https://unpkg.com/three@0.182.0/build/three.module.js";
 
 const scene = new THREE.Scene();
 
@@ -32,3 +32,4 @@ function animate() {
 }
 
 animate();
+*/
